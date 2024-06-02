49597dc3fdc5bae9d6eaa85809d2d1c436b8f36f
Yut8853 <factory0611@gmail.com>
2024-06-02 09:19:03 +0900

    first

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+import '../index.css'
+
+const MainContainer = () => {
+  return (
+    <div className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+      <h1 className='text-5xl'>Welcome to the Course!</h1>
+    </div>
+  )
+}
+
+export default MainContainer
